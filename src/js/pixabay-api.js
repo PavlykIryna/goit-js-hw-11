d8862bca564c665d235a4dac437489ed1d5f676a
PavlykIryna <155089011+PavlykIryna@users.noreply.github.com>
2024-08-09 20:04:55 +0200

    finish-02

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,21 @@
 const API_KEY = '45358562-a6f601b45a643dd81cb1b87d3';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
-      query
-    )}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  if (!response.ok) {
-    throw new Error('Failed to fetch images');
-  }
-  const data = await response.json();
-  return data.hits;
+export function fetchImages(query) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
+    query
+  )}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch images');
+      }
+      return response.json();
+    })
+    .then(data => data.hits)
+    .catch(error => {
+      console.error('Error:', error);
+      throw error;
+    });
 }
